8eaf385b4e40e9b3fd15afce0bd1477e274220b3
tpho216 <tpho216@gmail.com>
2022-01-15 22:22:17 +1100

    enhanced usage of selecting dom element through React useRef hooks

--- src/components/DroppableArea/DroppableArea.tsx
+++ src/components/DroppableArea/DroppableArea.tsx
@@ -1,23 +1,36 @@
-import React, {useContext, useEffect, useRef, useState} from 'react';
+import React, {useCallback, useContext, useEffect, useRef, useState} from 'react';
+import ReactDOM from 'react-dom';
 import {CursorContext} from "../Provider/CursorContextProvider";
 import './DroppableArea.css';
 
 const DroppableArea : React.FC = () => {
     const { cursor ,setCursor } = useContext(CursorContext);
-    const dropAreaRef = useRef(null);
-    const svgRef = useRef(null);
+    const containerRef = useRef(null);
+    const svgRef : any = useRef(null);
     const [hovered, setHovered] = useState(false);
-    const [dropX, setDropX] = useState(0);
-    const [dropY, setDropY] = useState(0);
+    const [dropX, setDropX] = useState(1000);
+    const [dropY, setDropY] = useState(1000);
+
+    useEffect(() => {
+        setTimeout(() => {
+            const containerElem = containerRef.current;
+            console.log("useEffect containerElem: ", containerElem);
+        }, 300)
+
+    },[]);
 
     const handleMouseDown = (event : any) => {
+        console.log("mouse down...");
         console.log("event.X: ", event.clientX);
         console.log("event.Y: ", event.clientY);
 
-        const dropArea = document.querySelector('.dropArea');
 
-        const dropAreaBBox = dropArea?.getBoundingClientRect();
-
+        const svgElem = svgRef.current;
+        console.log("Mouse down svgBBox: ", svgElem.getBoundingClientRect());
+        // const dropArea = document.querySelector('.dropArea');
+        // console.log(dropArea);
+        // const dropAreaBBox = dropArea?.getBoundingClientRect();
+        const dropAreaBBox = svgElem.getBoundingClientRect();
         if (dropAreaBBox) {
             console.log("dropArea X", dropAreaBBox.x);
             console.log("dropArea Y", dropAreaBBox.y);
@@ -29,31 +42,50 @@ const DroppableArea : React.FC = () => {
 
             console.log("mousePosInDropArea X: ", mousePosInDropArea.x);
             console.log("mousePosInDropArea Y: ", mousePosInDropArea.y);
-
-
-            const dropAreaElem = dropAreaRef.current;
-
+            toggleCursor();
             setDropX(mousePosInDropArea.x-25);
             setDropY(mousePosInDropArea.y-25);
         }
     }
 
-    const handleMouseOver = (event : any) => {
-        console.log("Mouse over");
-        setHovered(!hovered);
+
+    const toggleCursor = useCallback(() => {
+        setCursor(({active}) => ({active: !active}));
+    },[])
+
+
+    const handleMouseUp = () => {
+        console.log("Mouse up...");
+    }
+
+    const handleMouseEnter = () => {
+        console.log("Mouse entered...");
+        setHovered(true);
+    }
+
+    const handleMouseLeave = () => {
+        console.log("Mouse left...");
+        setHovered(false)
     }
 
 
+
     return (
-        <svg ref={svgRef}>
-            <rect
-                  className={hovered ? 'dropArea-active' : 'dropArea'}
-                  ref={dropAreaRef}
-                  onMouseOver={handleMouseOver}
-                  onMouseDown={handleMouseDown}
-                  width="100" height="100" fill="#050"/>
-            <rect width="30" height="30" x={dropX} y={dropY}/>
-        </svg>)
+        <div className="container" ref={containerRef}>
+            <svg ref={svgRef} width="500" height="500">
+                <rect
+                    id="dropArea"
+                    className={hovered ? 'dropArea-active' : 'dropArea'}
+                    onMouseDown={handleMouseDown}
+                    onMouseUp={handleMouseUp}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                    width="200"
+                    height="200"/>
+                <rect width="30" height="30" x={dropX} y={dropY}/>
+            </svg>
+        </div>
+      )
 }
 
 export default DroppableArea;
